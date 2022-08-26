b9609704a054fe2122996dc353445ddf5cd04e7b
gerriemarx <gerriemarx@gmail.com>
2022-08-26 13:23:05 +0200

    Created input and table

---
+++ src/components/SpinResultTable.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: ['spinResultHistory']
+}
+</script>
+
+<template>
+  <table>
+  <th>
+    <td>#</td>
+    <td>Result</td>
+  </th>
+  <tr v-for="(result, i) of spinResultHistory">
+    <td>{{ i + 1 }}</td>
+    <td>{{ result }}</td>
+  </tr>
+</table>
+</template>
---
+++ src/pages/Main.vue
@@ -0,0 +1,40 @@
+<script>
+import SpinResultTable from '../components/SpinResultTable.vue';
+
+export default {
+  components: {
+    SpinResultTable,
+  },
+  data() {
+    return {
+      spinResult: null,
+      spinResultHistory: [],
+    };
+  },
+  methods: {
+    captureSpinResult(event) {
+      if (this.spinResult == null || this.spinResult == undefined) return;
+
+      if (this.spinResult <= 36 && this.spinResult >= 0) {
+        this.spinResultHistory.push(this.spinResult);
+      }
+
+      this.spinResult = null;
+    },
+  },
+};
+</script>
+
+<template>
+  <label for="spinResultInput">Spin result: </label>
+  <input
+    id="spinResultInput"
+    type="number"
+    v-model="spinResult"
+    min="0"
+    max="36"
+    @keydown.enter="captureSpinResult"
+  />
+
+  <SpinResultTable :spin-result-history="spinResultHistory" />
+</template>
